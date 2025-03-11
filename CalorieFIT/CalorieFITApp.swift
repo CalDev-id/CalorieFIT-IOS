5fe8ce1742e063c028fdae90bfda91fcc151dc6a
Heical Chandra <haical31@gmail.com>
2025-03-11 17:39:03 +0700

    Initial commit

---
+++ CalorieFIT/CalorieFITApp.swift
@@ -0,0 +1,32 @@
+//
+//  CalorieFITApp.swift
+//  CalorieFIT
+//
+//  Created by Heical Chandra on 11/03/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CalorieFITApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CalorieFIT/Views/OnboardingView.swift
@@ -0,0 +1,24 @@
+//
+//  OnboardingView.swift
+//  CalorieFIT
+//
+//  Created by Heical Chandra on 11/03/25.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        VStack{
+            Text("Welcome To CalorieFIT")
+            
+            
+        }
+        
+        
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
